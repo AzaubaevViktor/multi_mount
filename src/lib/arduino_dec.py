8dd1fd3f53b6c76d9eba0837f2d1a7ed954a1854
Viktor Azaubaev <sarvash.inc@gmail.com>
2026-01-22 00:14:02 +0500

    Add lx200 command set

--- src/lib/arduino_dec.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-from serial_prims import SerialLineDevice
-from lx200_prims import LX200Client
-
-
-class LX200SerialClient(LX200Client):
-    def __init__(self, dev: SerialLineDevice):
-        super().__init__(dev, logger_name="dec.lx200")
