abebfbe8592b3e73e2ad0827837ced1d32167764
Viktor Azaubaev <sarvash.inc@gmail.com>
2026-02-04 21:39:35 +0500

    Task for tracking

--- src/skywatcher/skywatcher_lx200.py
+++ src/skywatcher/skywatcher_lx200.py
@@ -4,11 +4,43 @@ from .skywatcher import SkyWatcherMount, SlewMode
 
 
 class SkyWatcherLX200(LX200Base):
+    """
+    В режиме трекинга (монтировка движется со скоростью _STELLAR_SPEED):
+    - Ha остаётся постоянным
+
+    В режиме остановки:
+    - Ha остаёт (небесная сфера уходит вперёд) со скоростью _STELLAR_SPEED
+
+    В режиме slew:
+    - Ha меняется как (current_tick (текущее положение монтировки) - expected_tick (какое положение монтировки должно быть)) -> to Ha
+    
+    Гайдинг не должен ни на что влиять
+
+    Можно каждый раз смотреть на дельту между ожидаемыми тиками, полученными с монитровки и актуальными тиками
+
+    Т.е будет:
+    self.ra
+    self.last_mount_ra
+    self.last_update_s
+
+    при self.get_telescope_ra:
+    mount_ra = self.mount.get_telescope_ra
+    expected_ticks = self.last_tick + _STELLAR_SPEED * self.last_update_s
+    self.ra += (delta := (expected_ticks - mount_ra)) to lx200hours if delta > 0 else 0
+    update last_update_s and last_mount_ra
+
+    при set_telescope_ra
+    self.ra = value
+    last_mount_ra = self.mount.get_telescope_ra
+
+    """
     def __init__(self, mount: SkyWatcherMount) -> None:
         self.mount = mount
     
     def connect(self):
         self.mount.connect()
+        self.mount.set_telescope_ra(LX200Hours.from_hours(0))
+        self.mount.start_tracking()
 
     def get_telescope_ra(self) -> LX200Hours:
         return self.mount.get_telescope_ra()
