85017a37a725319b9afdf1b883f1a77a4c82b4c2
Viktor Azaubaev <sarvash.inc@gmail.com>
2026-03-13 20:26:23 +0500

    [AI] New main

--- src/__main__.py
+++ src/__main__.py
@@ -2,11 +2,12 @@ import threading
 
 from logging_setup import setup_logging
 from lx200.base_server import LX200SimpleServer
-from lx200.splitter import LX200Splitter
 from serial_wrapper.wrapper import SerialLine
-from skywatcher.skywatcher_lx200 import SkyWatcherLX200, SkyWatcherMount
-from tmc2209.tmc2209_adapter import TMC2209Adapter
-from tmc2209.tmc2209_lx200 import TMC2209LX200
+from sky.axis import AxisDEC, AxisRA
+from sky.combiner import Combiner
+from sky.lx200 import SkyLX200
+from skywatcher.motor import SkyWatcherMotor
+from tmc2209.motor import TMC2209Motor
 from web_control.web import MonitorServer
 
 
@@ -15,24 +16,21 @@ setup_logging()
 
 if __name__ == "__main__":
     sw_path = SerialLine.search("PL2303G")
-    sw_serial = SerialLine(sw_path, 115200, .05, "sw")
-    sw_ra_mount = SkyWatcherMount(sw_serial)
-    sw_lx200 = SkyWatcherLX200(sw_ra_mount)
+    sw_serial = SerialLine(sw_path, 115200, .05, "sw", terminator="\r")
+    sw_ra_motor = SkyWatcherMotor(sw_serial)
+    axis_ra = AxisRA(sw_ra_motor)
 
-
-    tmc_path = SerialLine.search("tty.usbserial") 
+    tmc_path = SerialLine.search("tty.usbserial")
     tmc_serial = SerialLine(tmc_path, 115200, 2, "tmc", terminator="\n")
-    tmc_dec_mount = TMC2209Adapter(tmc_serial)
-    tmc_lx200 = TMC2209LX200(tmc_dec_mount)
+    tmc_dec_motor = TMC2209Motor(tmc_serial)
+    axis_dec = AxisDEC(tmc_dec_motor)
 
-    splitter = LX200Splitter(
-        ra=sw_lx200,
-        dec=tmc_lx200,
-    )
+    combiner = Combiner(axis_ra, axis_dec)
+    sky_lx200 = SkyLX200(combiner)
 
-    web_server = MonitorServer({"skywatcher_ra": sw_ra_mount}, port=8765)
+    web_server = MonitorServer({"skywatcher_ra": sw_ra_motor}, port=8765)
     threading.Thread(target=web_server.serve_forever, name="WEB_CONTROL", daemon=True).start()
 
-    server = LX200SimpleServer(splitter)
+    server = LX200SimpleServer(sky_lx200)
 
     server.serve_forever()
